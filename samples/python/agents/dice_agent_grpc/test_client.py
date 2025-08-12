395992278157732a5b820a9261ca5c076ac45d8d
Krishna Thota <kcthota@google.com>
2025-08-12 15:33:30 -0700

    Add flag to skip fetching public agent card

--- samples/python/agents/dice_agent_grpc/test_client.py
+++ samples/python/agents/dice_agent_grpc/test_client.py
@@ -2,6 +2,7 @@ import logging  # Import the logging module
 
 from uuid import uuid4
 
+import asyncclick as click
 import grpc
 import httpx
 
@@ -18,48 +19,45 @@ from a2a.types import (
 from a2a.utils import proto_utils
 
 
-async def main() -> None:
+@click.command()
+@click.option('--agent-card-url', 'agent_card_url', default='http://localhost:11000')
+@click.option('--grpc-endpoint', 'grpc_endpoint', default=None)
+async def main(agent_card_url: str, grpc_endpoint: str | None) -> None:
     # Configure logging to show INFO level messages
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)  # Get a logger instance
 
-    # base_url is the location for the base agent_card being served by http server
-    base_url = 'http://localhost:11000'
+    if grpc_endpoint is None:
+        logger.info('gRPC endpoint not specified. Fetching public agent card from HTTP server')
+        # if grpc_url is not specified, try to fetch the public agent card
+        agent_card = await get_public_agent_card(agent_card_url)
+        base_url = agent_card.url
+    else:
+        logger.info('gRPC endpoint specified. Skip fetching the public agent card from HTTP server')
+        #if grpc endpoint is specific
+        base_url = grpc_endpoint
 
-    agent_card: AgentCard | None = None
-    async with httpx.AsyncClient() as httpx_client:
-        resolver = A2ACardResolver(
-            httpx_client=httpx_client,
-            base_url=base_url,
-        )
-        # Fetch the base agent card
-        agent_card = await resolver.get_agent_card()
 
-    if not agent_card:
-        raise ValueError('Agent card not found')
-
-    final_agent_card_to_use = agent_card
-
-    async with grpc.aio.insecure_channel(agent_card.url) as channel:
+    async with grpc.aio.insecure_channel(base_url) as channel:
         stub = a2a_pb2_grpc.A2AServiceStub(channel)
 
-        # if the base agent card has authenticated_extended_card flag set
         # use the gRPC channel to get the authenticated agent card
-        # use the authenticated agent card for interacting with the gRPC service
-        if agent_card.supports_authenticated_extended_card:
-            try:
-                logger.info(
-                    'Attempting to fetch authenticated agent card from grpc endpoint'
-                )
-                proto_card = await stub.GetAgentCard(a2a_pb2.GetAgentCardRequest())
-                logger.info('Successfully fetched agent card:')
-                logger.info(proto_card)
-                final_agent_card_to_use = proto_utils.FromProto.agent_card(
-                    proto_card
-                )
-            except Exception:
-                logging.exception('Failed to get authenticated agent card. Exiting.')
-                return
+        # in real-word applications, agent_card.supports_authenticated_extended_card flag
+        # specifies if authenticated card should be fetched.
+        # If an authenticated agent card is provided, client should use it for interacting with the gRPC service
+        try:
+            logger.info(
+                'Attempting to fetch authenticated agent card from grpc endpoint'
+            )
+            proto_card = await stub.GetAgentCard(a2a_pb2.GetAgentCardRequest())
+            logger.info('Successfully fetched agent card:')
+            logger.info(proto_card)
+            final_agent_card_to_use = proto_utils.FromProto.agent_card(
+                proto_card
+            )
+        except Exception:
+            logging.exception('Failed to get authenticated agent card. Exiting.')
+            return
 
 
         client = A2AGrpcClient(stub, agent_card=final_agent_card_to_use)
@@ -80,6 +78,21 @@ async def main() -> None:
 
         async for chunk in stream_response:
             logging.info(chunk.model_dump(mode='json', exclude_none=True))
+
+async def get_public_agent_card(agent_card_url: str) -> AgentCard:
+    agent_card: AgentCard | None = None
+    async with httpx.AsyncClient() as httpx_client:
+        resolver = A2ACardResolver(
+            httpx_client=httpx_client,
+            base_url=agent_card_url,
+        )
+        # Fetch the base agent card
+        agent_card = await resolver.get_agent_card()
+
+    if not agent_card:
+        raise ValueError('Public agent card not found')
+
+    return agent_card
 
 
 if __name__ == '__main__':
